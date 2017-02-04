d5e12595f7cdbec61a6e4aaf8cdfc3cb728026d1
Milan Stovicek <milan.stovicek@rocket-internet.de>
2017-02-04 11:31:48 +0100

    PrinterFactory unit test

---
+++ tests/Unit/Printer/PrinterFactoryTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Parser\Tests\Unit\Printer;
+
+use Parser\Printer\PrinterFactory;
+use Parser\Printer\PrinterInterface;
+use PHPUnit\Framework\TestCase;
+
+class PrinterFactoryTest extends TestCase
+{
+    public function testReturnedType()
+    {
+        $printerHumanMock = $this->getPrinterMock();
+        $printerJsonMock = $this->getPrinterMock();
+
+        $printerFactory = new PrinterFactory($printerHumanMock, $printerJsonMock);
+
+        $this->assertInstanceOf(
+            PrinterInterface::class,
+            $printerFactory->getPrinter(true)
+        );
+
+        $this->assertInstanceOf(
+            PrinterInterface::class,
+            $printerFactory->getPrinter(false)
+        );
+    }
+
+    public function testReturnedValue()
+    {
+        $printerHumanMock = $this->getPrinterMock();
+        $printerJsonMock = $this->getPrinterMock();
+
+        $printerFactory = new PrinterFactory($printerHumanMock, $printerJsonMock);
+
+        $this->assertEquals(
+            $printerJsonMock,
+            $printerFactory->getPrinter(true)
+        );
+
+        $this->assertEquals(
+            $printerHumanMock,
+            $printerFactory->getPrinter(false)
+        );
+    }
+
+    private function getPrinterMock(): PrinterInterface
+    {
+        /** @var \PHPUnit_Framework_MockObject_MockBuilder|PrinterInterface $mock */
+        $mock = $this->getMockBuilder(PrinterInterface::class)
+            ->setMethods(['print'])
+            ->getMock();
+
+        $mock->expects(self::any())
+            ->method('print')
+            ->willReturn('printing');
+
+        return $mock;
+    }
+}
