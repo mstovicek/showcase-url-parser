76b06089e07e47e55795c952818242df0c3898b4
Milan Stovicek <milan.stovicek@rocket-internet.de>
2017-02-04 11:09:13 +0100

    Php parser test cases

--- tests/Unit/Parser/PhpParserTest.php
+++ tests/Unit/Parser/PhpParserTest.php
@@ -2,6 +2,8 @@
 
 namespace Parser\Tests\Unit\Parser;
 
+use Parser\Entity\Argument;
+use Parser\Entity\Url;
 use Parser\Parser\PhpParser;
 
 class PhpParserTest extends \PHPUnit_Framework_TestCase
@@ -14,5 +16,71 @@ class PhpParserTest extends \PHPUnit_Framework_TestCase
     {
         $phpParser = new PhpParser();
         $phpParser->parse('http://');
+    }
+
+    /**
+     * @param string $url
+     * @param Url $expectedUrlEntity
+     *
+     * @dataProvider dataProviderTestParse
+     */
+    public function testParse(string $url, Url $expectedUrlEntity)
+    {
+        $phpParser = new PhpParser();
+
+        $this->assertEquals(
+            $expectedUrlEntity,
+            $phpParser->parse($url)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderTestParse(): array
+    {
+        return [
+            [
+                'http://php.net/manual/en/function.parse-url.php',
+                new Url(
+                    'http',
+                    'php.net',
+                    '/manual/en/function.parse-url.php',
+                    []
+                ),
+            ],
+            [
+                'https://bugs.php.net/report.php?bug_type=Documentation+problem&manpage=function.parse-url',
+                new Url(
+                    'https',
+                    'bugs.php.net',
+                    '/report.php',
+                    [
+                        new Argument('bug_type', 'Documentation problem'),
+                        new Argument('manpage', 'function.parse-url'),
+                    ]
+                ),
+            ],
+            [
+                'http://192.168.1.1?user=admin',
+                new Url(
+                    'http',
+                    '192.168.1.1',
+                    null,
+                    [
+                        new Argument('user', 'admin'),
+                    ]
+                ),
+            ],
+            [
+                'file:///tmp/empty.html',
+                new Url(
+                    'file',
+                    null,
+                    '/tmp/empty.html',
+                    []
+                ),
+            ],
+        ];
     }
 }
